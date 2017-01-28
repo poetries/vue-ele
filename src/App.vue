36e878f5ea1b3a80f071b1b64d0d648f92389c9a
poetries <jingguanliuye@gmail.com>
2017-01-28 19:22:38 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <div class="tab">
+        <div class="tab-item">商品 </div>
+        <div class="tab-item">评论 </div>
+        <div class="tab-item">商家 </div>
+    </div>
+    <div class="content">
+        i am content
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import header from './components/header/header';
+
+  export default {
+    components: {
+      'v-header': header
+    }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    #App
+      .tab
+        display: flex
+        width: 100%
+        height: 40px
+        line-height: 40px
+        .tab-item
+          flex: 1
+          text-align: center
+
+</style>
